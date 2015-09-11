1bf0d01467469b4a21a4bbb9b8b15f1fe4f50c3d
Koperfild <avalancher2@rambler.ru>
2015-09-11 06:20:04 +0300

    Debug step. Server and client apps are not yet separated

---
+++ DreamTeamTask2/ActionResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamTeamTask2
+{
+    [Serializable]
+    public struct ActionResult<ActionTypeStruct>
+        where ActionTypeStruct : IEquatable<ActionTypeStruct>
+    {
+        private readonly ActionTypeStruct _identificator;
+        private readonly bool _result;
+
+        public ActionResult(ActionTypeStruct identificator, bool Result)
+        {
+            _identificator = identificator;
+            _result = Result;
+        }
+
+        public ActionTypeStruct Identificator
+        {
+            get { return _identificator; }
+        }
+
+        public bool Result
+        {
+            get { return _result; }
+        }
+    }
+}
---
+++ DreamTeamTask2/ActionTypeStruct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamTeamTask2
+{
+    public enum ActionType
+    {
+        Increment,
+        Decrement,
+        Flush
+    }
+    [Serializable]
+    public struct ActionTypeStruct : IEquatable<ActionTypeStruct>
+    {
+        private readonly ActionType _type;
+
+        public ActionTypeStruct(ActionType type)
+        {
+            _type = type;
+        }
+
+        public ActionType Type
+        {
+            get { return _type; }
+        }
+
+        public bool Equals(ActionTypeStruct other)
+        {
+            return _type == other._type;
+        }
+    }
+}
